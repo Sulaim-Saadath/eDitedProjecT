b873dd31a264b303aaae5d1b3008eb423ff9f5b0
Sulaim-Saadath <avoor2005@gmail.com>
2026-07-01 12:28:53 +0530

    commit

---
+++ peoject4/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sum from './Sum'
+
+// App.jsx
+export default function App() {
+  return (
+    <div>
+       <h1>Hello World</h1>
+       <Sum /> 
+    </div>
+  )
+}
